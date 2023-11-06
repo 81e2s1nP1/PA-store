92c79f7066c1cf76c13539dbcd19fca11188480a
81e2s1nP1 <tranphuocan070197@gmail.com>
2023-11-06 09:01:49 +0700

    update web-banhang

--- src/main/java/com/pa/repository/Accout_Repository.java
+++ src/main/java/com/pa/repository/Accout_Repository.java
@@ -1,12 +1,12 @@
-package com.pa.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-import com.pa.model.Account;
-
-@Repository
-public interface Accout_Repository extends PagingAndSortingRepository<Account, Integer> {
-	public Account findById(int id);
-	public Account findByEmail(String email);
-}
+	package com.pa.repository;
+	
+	import org.springframework.data.repository.PagingAndSortingRepository;
+	import org.springframework.stereotype.Repository;
+	
+	import com.pa.model.Account;
+	
+	@Repository
+	public interface Accout_Repository extends PagingAndSortingRepository<Account, Integer> {
+		public Account findById(int id);
+		public Account findByEmail(String email);
+	}
